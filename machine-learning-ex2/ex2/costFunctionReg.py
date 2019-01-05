412a0d9e00e725640c6e97f30f71816b2cd836a5
HaTsuNeMK <610046415@qq.com>
2019-01-05 22:46:28 +0800

    修改grad

--- machine-learning-ex2/ex2/costFunctionReg.py
+++ machine-learning-ex2/ex2/costFunctionReg.py
@@ -17,5 +17,12 @@ def cost_function_reg(theta, X, y, lmd):
 
     # ===========================================================
     cost = (1 / m) * np.sum(-y * np.log(sigmoid(X.dot(theta))) - (1 - y) * np.log(1 - sigmoid(X.dot(theta)))) + (lmd / (2 * m)) * np.sum(theta ** 2)
-    grad = (1 / m) * X.T.dot(sigmoid(X.dot(theta)) - y) + (lmd / m) * theta
+    for i in range(m):
+        grad[0] += (sigmoid(X[i, :].dot(theta)) - y[i]) * 1
+    # grad[0] = grad[0] * (1 / m)
+    for j in range(1, theta.shape[0]):
+        for i in range(m):
+            grad[j] = grad[j] + (sigmoid(X[i, :].dot(theta)) - y[i]) * X[i, j]
+        grad[j] = grad[j] + lmd * theta[j]
+    grad = grad * (1 / m)   
     return cost, grad
